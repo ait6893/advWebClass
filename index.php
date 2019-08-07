b1656cb4626d5bd23ce8e0e702d91f07654002b6
6893 <6893@ait-sydney.net.au>
2019-08-07 16:44:56 +1000

    added product detail

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $products = new Product();
 $products_result = $products -> getProducts();
 
 //create twig loader
-$loader = new Twig_Loader_FilesystemLoader('templates');
+//$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new Twig_Loader_Filesystem('templates');
 
 //create twig environment
 $twig = new Twig_Environment($loader);
@@ -15,8 +16,9 @@ $twig = new Twig_Environment($loader);
 //load a twig template
 $template = $twig -> load('home.twig');
 
-//pass value to twig
-echo $template -> render([
+//pass values to twig
+echo $template -> render
+([
     'products' => $products_result,
     'title' => 'Hello shop'
 ]);
